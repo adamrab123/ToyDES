cb2f5ea56771c93295ea532602e80a5132cfe938
adamrab123 <arabinowitz10@gmail.com>
2018-09-19 18:28:32 -0400

    added comments to server and client files

--- client.py
+++ client.py
@@ -4,41 +4,39 @@ import sys
 from time import sleep
 import library
 
-
 def Main():
         host = "127.0.0.1"
         port = 5000
-         
         mySocket = socket.socket()
         mySocket.connect((host,port))
          
         
         message = input("Enter the message you want to encrypt -> ")
-
+        #encrypting the message using DES
         finalEncryptedMessage = library.encrypt(message)
         print("Encrypted message = " + finalEncryptedMessage)
 
-
+        #have the communication go on forever
         while message != 'q':
 
+                #prints the pretty loading bar
                 library.sending()
 
+                #encrypting the message
                 finalEncryptedMessage = library.encrypt(message)
-            
+                #sending the message
                 mySocket.send(finalEncryptedMessage.encode())
+                #receiving the response from the other user
                 data = mySocket.recv(1024).decode()
                 print("Received from server = " + data)
-                
+                #decrypting the other user's message
                 decryptedMessage = library.decrypt(data)
-
                 if not data:
                         break
                 print ("Decrypted Message = " + str(decryptedMessage))
-
                 print("\n")
-                 
+                #setting up the message all over again....
                 message = input("Enter the message you want to encrypt -> ")
-
                 finalEncryptedMessage = library.encrypt(message)
                 print("Encrypted message = " + finalEncryptedMessage)
                  
--- server.py
+++ server.py
@@ -2,42 +2,41 @@ import socket
 import des
 import library
 
+#this will serve as the "server" for our implementation
+
 def Main():
     host = "127.0.0.1"
     port = 5000
-     
+    #necessary to initiate the server
     mySocket = socket.socket()
     mySocket.bind((host,port))
 
     print("Waiting for connection.....")
-     
+    #listens for a user to connect
     mySocket.listen(2)
-
+    #getting the user's connection info
     conn, addr = mySocket.accept()
     print ("Connection from: " + str(addr))
 
-    
     while True:
+            #receiving the response from the other user
             data = conn.recv(1024).decode()
-            
             print("Received from client = " + data)
-            
+            #decrypting the other user's message
             decryptedMessage = library.decrypt(data)
-
             if not data:
                     break
             print ("Decrypted Message = " + str(decryptedMessage))
-
             print("\n")
-            
             message = input("Enter the message you want to encrypt -> ")
+            #encrypting the message using DES
             finalEncryptedMessage = library.encrypt(message)
             print("Encrypted message = " + finalEncryptedMessage)
-
+            #prints the pretty loading bar
             library.sending()
+            #sending the message
             conn.send(finalEncryptedMessage.encode())
-
-             
+ 
     conn.close()
      
 if __name__ == '__main__':
